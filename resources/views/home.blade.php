9676d5317c1723326bed516a2e95f355a7566279
javi <javieraguilararribas@yahoo.es>
2021-01-22 20:03:31 +0100

    Controllador Like añadido y metodos de sistema de Likes hecho

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -36,7 +36,22 @@
                         <p>{{ $image->description }}</p>
                     </div>
                     <div class="likes">
-                        <img src="{{ asset('img/heart-black.png') }}" />
+
+                        <!-- Comprobar si el usuario le dio like a la imagen -->
+                        <?php $user_like = false; ?>
+                        @foreach ($image->likes as $like)
+                            @if($like->user->id == Auth::user()->id)
+                                <?php $user_like = true; ?>
+                            @endif
+                        @endforeach
+
+                        @if($user_like)
+                            <img src="{{ asset('img/heart-red.png') }}" class="btn-like" />
+                        @else
+                            <img src="{{ asset('img/heart-black.png') }}" class="btn-dislike" />
+                        @endif
+                        <span class="number_likes">{{ count($image->likes) }}</span>
+
                     </div>
                     <div class="comments">
                         <a href="" class="btn btn-sm btn-warning btn-comments">
--- routes/web.php
+++ routes/web.php
@@ -39,3 +39,5 @@ Route::get('/image/file/{filename}', 'ImageController@getImage')->name('image.fi
 Route::get('/imagen/{id}', 'ImageController@detail')->name('image.detail');
 Route::post('/comment/save', 'CommentController@store')->name('comment.save');
 Route::get('/comment/delete/{id}', 'CommentController@delete')->name('comment.delete');
+Route::get('/like/{image_id}', 'LikeController@like')->name('like.save');
+Route::get('/dislike/{image_id}', 'LikeController@dislike')->name('like.delete');
